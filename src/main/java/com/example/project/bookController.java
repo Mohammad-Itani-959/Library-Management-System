a864662b7f4bdfc91c7e71341e48d2ed24ea6be0
phpproject2023 <alaakanso120@gmail.com>
2023-04-09 21:09:35 +0300

    ?

---
+++ src/main/java/com/example/project/bookController.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class bookController {
+    @FXML
+    private ImageView bookImage;
+    @FXML
+    private Text bookTitle;
+    @FXML
+    private Text bookAuthor;
+
+}
