49d6c8a31227680bcbb5e71e15ea5a3f7869060e
phpproject2023 <alaakanso120@gmail.com>
2023-04-23 13:19:39 +0300

    .

--- src/main/java/com/example/project/librarianController.java
+++ src/main/java/com/example/project/librarianController.java
@@ -1,5 +1,23 @@
 package com.example.project;
 
-public class librarianController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class librarianController {
+    public void Logout(ActionEvent actionEvent) throws SQLException, IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("entry.fxml"));
+        Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        AnchorPane root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setFullScreen(true);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
