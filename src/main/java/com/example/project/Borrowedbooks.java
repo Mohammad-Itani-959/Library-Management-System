2346ccac3fde118593ceca428b20bc2118ba21d2
phpproject2023 <alaakanso120@gmail.com>
2023-04-23 16:36:21 +0300

    .

---
+++ src/main/java/com/example/project/Borrowedbooks.java
@@ -0,0 +1,34 @@
+package com.example.project;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Borrowedbooks {
+
+    public ProxyUser proxyUser;
+    @javafx.fxml.FXML
+    @FXML
+    private Label email;
+    public void Logout(ActionEvent actionEvent) throws SQLException, IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("entry.fxml"));
+        Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        AnchorPane root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setFullScreen(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void setProxyUser(ProxyUser proxyUser){
+        this.proxyUser = proxyUser;
+        this.email.setText(proxyUser.getRealUser().getEmail());
+    }
+}
