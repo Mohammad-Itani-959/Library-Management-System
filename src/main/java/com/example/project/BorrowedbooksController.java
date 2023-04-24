61def52b687f51732d6912a5eff46993d57a4978
phpproject2023 <alaakanso120@gmail.com>
2023-04-24 14:13:47 +0300

    x

--- src/main/java/com/example/project/BorrowedbooksController.java
+++ src/main/java/com/example/project/BorrowedbooksController.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.SQLException;
 
-public class Borrowedbooks {
+public class BorrowedbooksController {
 
     public ProxyUser proxyUser;
     @javafx.fxml.FXML
