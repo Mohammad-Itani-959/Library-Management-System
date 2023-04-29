b70bb06ae1e3dee46e27aa45df20a35547ba28af
phpproject2023 <alaakanso120@gmail.com>
2023-04-29 10:07:37 +0300

    .

--- src/main/java/com/example/project/Database.java
+++ src/main/java/com/example/project/Database.java
@@ -13,7 +13,7 @@ public class Database {
     Statement statement ;
 
     public Database() throws SQLException {
-        this.connection= DriverManager.getConnection(String.format("jdbc:mysql://localhost:3306/%s", "guiprroject"), "root", "");
+        this.connection= DriverManager.getConnection(String.format("jdbc:mysql://localhost:3306/%s", "guiprroject"), "root", "AlaaKanso2002.@.");
         this.statement = this.connection.createStatement();
     }
     public void createTables() throws SQLException{
